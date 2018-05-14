b998b228552631db92ee3aaacabd1030d3f3ae1c
Darnel Kumar <darnel.kumar91@gmail.com>
2018-05-14 23:36:30 +0100

    More firebase stuff

--- Assets/js/FirebaseSetup.js
+++ Assets/js/FirebaseSetup.js
@@ -1,5 +1,9 @@
+// Button IDs
+// #LoginButton
+// #LogOutButton
+
 console.log("Starting Firebase...");
-var _GlobalData = {
+var GlobalData = {
     FirebaseConfig: {
         apiKey: "AIzaSyA8a6WnwAyOeq61gmtKwS6Oa0TNdY6M5Bo",
         authDomain: "vladb-todo.firebaseapp.com",
@@ -7,10 +11,13 @@ var _GlobalData = {
         projectId: "vladb-todo",
         storageBucket: "",
         messagingSenderId: "180513059705"
-    }
+    },
+    UserData: null
 };
 // Initialize Firebase
-firebase.initializeApp(_GlobalData.FirebaseConfig);
+firebase.initializeApp(GlobalData.FirebaseConfig);
+var Database = firebase.database();
+var FB_DATA = {};
 firebase.auth().useDeviceLanguage();
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -18,12 +25,84 @@ firebase.auth().onAuthStateChanged(function(user) {
         if (location.pathname == "/todo/login.html") {
             location.pathname = "/todo/index.html";
         } else {
+            var OUT = document.querySelector("#LogOutButton");
+            if (OUT) {
+                OUT.addEventListener("click", function() {
+                    firebase
+                        .auth()
+                        .signOut()
+                        .then(function() {
+                            location.pathname = "/todo/login.html";
+                        })
+                        .catch(function(e) {
+                            alert(e);
+                            console.warn(e);
+                        });
+                });
+            }
+            // Do logged in firebase stuff
         }
     } else {
         console.log("User Log In Required.");
         if (location.pathname == "/todo/index.html") {
             location.pathname = "/todo/login.html";
         } else {
+            var LB = document.querySelector("#LoginButton");
+            if (LB) {
+                LB.addEventListener("click", function() {
+                    var provider = new firebase.auth.GoogleAuthProvider();
+                    provider.addScope("profile");
+                    provider.addScope("email");
+                    firebase
+                        .auth()
+                        .signInWithPopup(provider)
+                        .then(function(result) {
+                            if (result.credential) {
+                                FB_DATA["Token"] =
+                                    result.credential.accessToken;
+                            }
+                            FB_DATA["User"] = result.user;
+                            FB_DATA[
+                                "CurrentUser"
+                            ] = firebase.auth().currentUser;
+                            FB_DATA["UID"] = FB_DATA["CurrentUser"].uid;
+                            firebase
+                                .database()
+                                .ref("/Users/" + FB_DATA["UID"])
+                                .once("value")
+                                .then(function(snapshot) {
+                                    if (!snapshot.val()) {
+                                        GlobalData.UserData = {
+                                            UID: FB_DATA["UID"],
+                                            Email: FB_DATA["CurrentUser"].email,
+                                            DisplayName:
+                                                FB_DATA["CurrentUser"]
+                                                    .displayName,
+                                            PhoneNumber:
+                                                FB_DATA["CurrentUser"]
+                                                    .phoneNumber,
+                                            ProfilePicture:
+                                                FB_DATA["CurrentUser"].photoURL
+                                        };
+                                        firebase
+                                            .database()
+                                            .ref("/Users/" + FB_DATA["UID"])
+                                            .set(UserData);
+                                    } else {
+                                        GlobalData.UserData = snapshot.val();
+                                    }
+                                });
+                        })
+                        .catch(function(e) {
+                            FB_DATA["Error"] = {
+                                Code: e.code,
+                                MSG: e.message,
+                                Email: e.email,
+                                Credential: e.credential
+                            };
+                        });
+                });
+            }
         }
     }
 });
